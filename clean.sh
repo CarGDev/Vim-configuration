f70e02a5f2a5ca03bb8d04bbf068a85d557a1a69
Carlos <carlos.gutierrez@tortoise.dev>
2021-09-20 13:23:37 -0500

    updating

--- clean.sh
+++ clean.sh
@@ -1,2 +1,7 @@
 sudo apt-get autoclean || { echo "clean failed" ; exit 1; } >> error.txt
+echo autoclean >> date.txt
+date >> date.txt
 sudo apt-get autoremove || { echo "remove failed" ; exit 1; } >> error.txt
+echo autoremove >> date.txt
+date >> date.txt
+
--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 sudo apt-get update || { echo "update failed" ; exit 1; } >> error.txt
+echo update >> date.txt
+date >> date.txt
 sudo apt-get upgrade -y || { echo "upgrade failed" ; exit 1; } >> error.txt
+echo upgrade >> date.txt
+date >> date.txt
 sudo apt-get dist-upgrade -y || { echo "dist upgrade failed" ; exit 1; } >> error.txt
-
+echo dist upgrade >> date.txt
 date >> date.txt
